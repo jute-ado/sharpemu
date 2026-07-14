eb73a8cd8864df137c5d2e7906a6dff470320989
jute-ado <304153664+jute-ado@users.noreply.github.com>
2026-07-14 13:43:06 +0200

    Test timed semaphore waits

--- src/SharpEmu.Libs.Tests/KernelSyncPointerCompatibilityTests.cs
+++ src/SharpEmu.Libs.Tests/KernelSyncPointerCompatibilityTests.cs
@@ -1,6 +1,7 @@
 // Copyright (C) 2026 SharpEmu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 using SharpEmu.HLE;
@@ -165,6 +166,165 @@ public sealed class KernelSyncPointerCompatibilityTests
             }
 
             FreeTrackedBuffer(context, waiterCountAddress);
+            FreeTrackedBuffer(context, timeoutAddress);
+            FreeTrackedBuffer(context, handleAddress);
+            FreeTrackedBuffer(context, nameAddress);
+        }
+    }
+
+    [Fact]
+    public void TimedSemaphoreWaitWakesBeforeDeadlineAndUpdatesRemainingTimeout()
+    {
+        var context = new CpuContext(new FakeGuestMemory(), Generation.Gen5);
+        var nameAddress = AllocateTrackedBuffer(context, 32);
+        var handleAddress = AllocateTrackedBuffer(context, sizeof(uint));
+        var timeoutAddress = AllocateTrackedBuffer(context, sizeof(uint));
+        uint handle = 0;
+        var previousGuestThread = GuestThreadExecution.EnterGuestThread(0x1234);
+
+        try
+        {
+            WriteCString(nameAddress, "timed-wake-semaphore");
+            context[CpuRegister.Rdi] = handleAddress;
+            context[CpuRegister.Rsi] = nameAddress;
+            context[CpuRegister.Rdx] = 0;
+            context[CpuRegister.Rcx] = 0;
+            context[CpuRegister.R8] = 2;
+            context[CpuRegister.R9] = 0;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelCreateSema(context));
+            handle = unchecked((uint)Marshal.ReadInt32((nint)handleAddress));
+
+            const int timeoutMicros = 5_000_000;
+            Marshal.WriteInt32((nint)timeoutAddress, timeoutMicros);
+            context[CpuRegister.Rdi] = handle;
+            context[CpuRegister.Rsi] = 1;
+            context[CpuRegister.Rdx] = timeoutAddress;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelWaitSema(context));
+
+            Assert.True(GuestThreadExecution.TryConsumeCurrentThreadBlock(
+                out var reason,
+                out _,
+                out var hasContinuation,
+                out var wakeKey,
+                out var resumeHandler,
+                out var wakeHandler,
+                out var deadlineTimestamp));
+            Assert.Equal("sceKernelWaitSema", reason);
+            Assert.False(hasContinuation);
+            Assert.Equal($"kernel_sema:0x{handle:X8}", wakeKey);
+            Assert.NotNull(resumeHandler);
+            Assert.NotNull(wakeHandler);
+            Assert.True(deadlineTimestamp > Stopwatch.GetTimestamp());
+
+            context[CpuRegister.Rdi] = handle;
+            context[CpuRegister.Rsi] = 1;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelSignalSema(context));
+            Assert.True(wakeHandler!());
+            Assert.Equal((int)OrbisGen2Result.ORBIS_GEN2_OK, resumeHandler!());
+
+            var remainingTimeout = Marshal.ReadInt32((nint)timeoutAddress);
+            Assert.InRange(remainingTimeout, 1, timeoutMicros);
+
+            context[CpuRegister.Rdi] = handle;
+            context[CpuRegister.Rsi] = 1;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_ERROR_BUSY,
+                KernelSemaphoreCompatExports.KernelPollSema(context));
+        }
+        finally
+        {
+            GuestThreadExecution.RestoreGuestThread(previousGuestThread);
+            if (handle != 0)
+            {
+                context[CpuRegister.Rdi] = handle;
+                _ = KernelSemaphoreCompatExports.KernelDeleteSema(context);
+            }
+
+            FreeTrackedBuffer(context, timeoutAddress);
+            FreeTrackedBuffer(context, handleAddress);
+            FreeTrackedBuffer(context, nameAddress);
+        }
+    }
+
+    [Fact]
+    public void TimedSemaphoreWaitExpiresAndDoesNotConsumeLateSignal()
+    {
+        var context = new CpuContext(new FakeGuestMemory(), Generation.Gen5);
+        var nameAddress = AllocateTrackedBuffer(context, 32);
+        var handleAddress = AllocateTrackedBuffer(context, sizeof(uint));
+        var timeoutAddress = AllocateTrackedBuffer(context, sizeof(uint));
+        uint handle = 0;
+        var previousGuestThread = GuestThreadExecution.EnterGuestThread(0x5678);
+
+        try
+        {
+            WriteCString(nameAddress, "timed-expiry-semaphore");
+            context[CpuRegister.Rdi] = handleAddress;
+            context[CpuRegister.Rsi] = nameAddress;
+            context[CpuRegister.Rdx] = 0;
+            context[CpuRegister.Rcx] = 0;
+            context[CpuRegister.R8] = 2;
+            context[CpuRegister.R9] = 0;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelCreateSema(context));
+            handle = unchecked((uint)Marshal.ReadInt32((nint)handleAddress));
+
+            Marshal.WriteInt32((nint)timeoutAddress, 1_000);
+            context[CpuRegister.Rdi] = handle;
+            context[CpuRegister.Rsi] = 1;
+            context[CpuRegister.Rdx] = timeoutAddress;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelWaitSema(context));
+
+            Assert.True(GuestThreadExecution.TryConsumeCurrentThreadBlock(
+                out _,
+                out _,
+                out _,
+                out _,
+                out var resumeHandler,
+                out var wakeHandler,
+                out var deadlineTimestamp));
+            Assert.NotNull(resumeHandler);
+            Assert.NotNull(wakeHandler);
+            Assert.True(SpinWait.SpinUntil(
+                () => Stopwatch.GetTimestamp() >= deadlineTimestamp,
+                TimeSpan.FromSeconds(1)));
+
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_ERROR_TIMED_OUT,
+                resumeHandler!());
+            Assert.Equal(0, Marshal.ReadInt32((nint)timeoutAddress));
+
+            context[CpuRegister.Rdi] = handle;
+            context[CpuRegister.Rsi] = 1;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelSignalSema(context));
+            Assert.True(wakeHandler!());
+
+            context[CpuRegister.Rdi] = handle;
+            context[CpuRegister.Rsi] = 1;
+            Assert.Equal(
+                (int)OrbisGen2Result.ORBIS_GEN2_OK,
+                KernelSemaphoreCompatExports.KernelPollSema(context));
+        }
+        finally
+        {
+            GuestThreadExecution.RestoreGuestThread(previousGuestThread);
+            if (handle != 0)
+            {
+                context[CpuRegister.Rdi] = handle;
+                _ = KernelSemaphoreCompatExports.KernelDeleteSema(context);
+            }
+
             FreeTrackedBuffer(context, timeoutAddress);
             FreeTrackedBuffer(context, handleAddress);
             FreeTrackedBuffer(context, nameAddress);
